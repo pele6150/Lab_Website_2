9f89385377987c133d02e30d128efe1226b9b838
pele6150 <59934024+pele6150@users.noreply.github.com>
2020-02-20 16:27:17 -0700

    Finished my_scripts.js

--- resources/js/my_scripts.js
+++ resources/js/my_scripts.js
@@ -22,74 +22,107 @@ var players = [{name:"John Doe", img: "../resources/img/player1.jpg", alt:"Image
 		viewStudentStats(id, toggle) method
 			parameters:
 				id - The css id of the html tag being updated.
-				toggle - 
+				toggle -
 					0 - hide the html tag
 					1 - make the html tag visible
-			
+
 			purpose: This method will accept the id of an html tag and a toggle value.
-					 The method will then set the html tag's css visibility and height.  
+					 The method will then set the html tag's css visibility and height.
 					 To hide the html tag (toggle - 0), the visibility will be set to hidden and
-					 the height will be set to 0.  
+					 the height will be set to 0.
 					 To reveal the html tag (toggle - 1), the visibility will be set to visible and
 					 the height will be set to auto.
 */
-				
+
+function viewStudentStats(id, toggle){
+	if(toggle){
+		document.getElementById(id).style.visibility = "visible";
+		document.getElementById(id).style.height = "auto";
+	}
+	else{
+		document.getElementById(id).style.visibility = "hidden";
+		document.getElementById(id).style.height = "0px";
+	}
+}
+
 /*
-	Home Page: 
+	Home Page:
 		changeColor(color) method
-			parameter: 
+			parameter:
 				color- A css color
-				
-			purpose: This method will set the html body's background color to the 
+
+			purpose: This method will set the html body's background color to the
 					 provided parameter.
 */
+
+function changeColor(color){
+	document.body.style.backgroundColor = color;
+}
 
 
 /*
 	Football Season Stats Page:
 		loadStatsPage method:
 			parameters: none
-			
-			purpose: This method will iterate through the stats table and 
+
+			purpose: This method will iterate through the stats table and
 					 do the following:
 						1. Read through each row of the table & determine which team won
 						   the game.
-						
+
 						2. Update the winner column to the name of the winning team.
-						
+
 						3. Keep track of the number of wins/losses for the Buffs.
-						
+
 						4. Update the second table to show the total number of wins/losses for the Buffs.
 */
+
+function loadStatsPage(){
+	var statsTab = document.getElementById("stats_table");
+	var cuWinCount = 0;
+	var cuLossCount = 0;
+	for(i = 2; i < statsTab.rows.length; i++){
+		if(statsTab.rows.item(i).cells.item(2).innerHTML > statsTab.rows.item(i).cells.item(3).innerHTML){
+			statsTab.rows.item(i).cells.item(4).innerHTML = "CU Boulder";
+			cuWinCount++;
+		}
+		else{
+			statsTab.rows.item(i).cells.item(4).innerHTML = statsTab.rows.item(i).cells.item(1).innerHTML;
+			cuLossCount++;
+		}
+	}
+	document.getElementById("wins").innerHTML = cuWinCount;
+	document.getElementById("losses").innerHTML = cuLossCount;
+}
 
 /*
 	Football Player Information Page
 		loadPlayersPage method:
 			parameters: none
-			
-			purpose: This method will populate the dropdown menu to allow the 
+
+			purpose: This method will populate the dropdown menu to allow the
 					 user to select which player's information to view.
-					 
+
 					 To handle this, you will need to iterate through the players array
 					 and do the following for each player:
 						1. Create an anchor tag
-						2. Set the href to "#", this will make sure the 
+						2. Set the href to "#", this will make sure the
 							anchor tag doesn't change pages
-						3. Set the onclick to call switchPlayers method 
+						3. Set the onclick to call switchPlayers method
 							(this will need to pass in the index inside the players array)
 						4. Set the anchor tag's text to the player's name.
-						
+
 					After setting all of the anchor tags, update the innerHTML of the dropdown menu.
 					As a note, the id for the dropdown menu is player_selector.
-		
+
 		switchPlayers(playerNum) method:
-			parameters: 
+			parameters:
 				playerNum - The index of the football player in the players array.
-			
+
 			purpose:
 				This method will update the the spans on the player's information pageX
 				and calculate the average passing, rushing, and receiving yards.
-				
+
 				Span ids:
 					p_year     - the player's year in college
 					p_major    - the player's major in college
@@ -98,11 +131,35 @@ var players = [{name:"John Doe", img: "../resources/img/player1.jpg", alt:"Image
 					p_yards    - the number of passing yards
 					r_yards    - the number of rushing yards
 					rec_yards  - the number of receiving yards
-					
+
 					Calculated values:
 					  avg_p_yards   - the average number of passing yards for the player's Buff career
 					  avg_r_yards   - the average number of rushing yards for the player's Buff career
 					  avg_rec_yards - the average number of receiving yards for the player's Buff career
 */
-				
 
+function loadPlayersPage(){
+	var currPlayer;
+	for(i = 0; i < players.length; i++){
+		currPlayer = document.createElement('a');
+		currPlayer.href = "#";
+		currPlayer.index = i;
+		currPlayer.onclick = function() {switchPlayers(this.index)};
+		currPlayer.text = players[i].name;
+		currPlayer.className = "dropdown-item";
+		document.getElementById("player_selector").appendChild(currPlayer);
+	}
+}
+
+function switchPlayers(playerNum){
+	document.getElementById("p_year").innerHTML = players[playerNum].year;
+	document.getElementById("p_major").innerHTML = players[playerNum].major;
+	document.getElementById("g_played").innerHTML = players[playerNum].games_played;
+	document.getElementById("player_img").innerHTML = players[playerNum].img;
+	document.getElementById("p_yards").innerHTML = players[playerNum].pass_yards;
+	document.getElementById("r_yards").innerHTML = players[playerNum].rushing_yards;
+	document.getElementById("rec_yards").innerHTML = players[playerNum].receiving_yards;
+	document.getElementById("avg_p_yards").innerHTML = parseInt(players[playerNum].pass_yards / players[playerNum].games_played);
+	document.getElementById("avg_r_yards").innerHTML = parseInt(players[playerNum].rushing_yards / players[playerNum].games_played);
+	document.getElementById("avg_rec_yards").innerHTML = parseInt(players[playerNum].receiving_yards / players[playerNum].games_played);
+}
